f225b3155bf6fb2572cb9a6a667484dc29ae25aa
Roman Zhuravel <zhuravel.r@centrobill.com>
2019-04-22 11:27:15 +0300

    added build.sh

--- builder-pattern/build.sh
+++ builder-pattern/build.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+echo Building docker-multi-stage-builds/builder-pattern:build
+
+docker build -t docker-multi-stage-builds/builder-pattern:build . -f Dockerfile.build
+
+docker container create --name extract docker-multi-stage-builds/builder-pattern:build
+
+docker container cp extract:/go/src/github.com/zhooravell/docker-multi-stage-builds/builder-pattern/app ./app
+
+docker container rm -f extract
+
+echo Building docker-multi-stage-builds/builder-pattern:latest
+
+docker build --no-cache -t docker-multi-stage-builds/builder-pattern:latest .
+
+rm ./app
